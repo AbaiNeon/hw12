9a665e8b70b2a515fffb70535a4900d7f187058c
Abai <abai2010@bk.ru>
2017-12-26 23:17:21 +0600

    finished

---
+++ hw12/ListExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw12
+{
+    public static class ListExtension
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int sizeOfList = list.Count;
+
+            while (sizeOfList > 1)
+            {
+                sizeOfList--;
+                int randomNumber = rng.Next(sizeOfList + 1);
+                T buffer = list[randomNumber];
+                list[randomNumber] = list[sizeOfList];
+                list[sizeOfList] = buffer;
+            }
+        }
+    }
+}
